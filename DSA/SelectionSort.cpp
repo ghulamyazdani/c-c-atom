3e4f91bdb4d93e19c4eedb1697bd7f8110222572
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2021-07-25 20:16:43 +0530

    Cpp

---
+++ DSA/SelectionSort.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+int SelectionSort(int *,int);
+int main(int argc, char const *argv[])
+{
+    int arr[20],num;
+    cout<<"Enter Number of elem array";
+    cin>>num;
+    cout<<"Enter al the elements of the array";
+    for (size_t i = 0; i < num; i++)
+    {
+        cin>>arr[i];
+    }
+    SelectionSort(arr,num);
+    return 0;
+}
+int SelectionSort(int a[],int n)
+{ 
+    int temp,small,pos;
+    for (size_t i = 0; i < n; i++)
+    {
+        small=a[i];
+        pos=i;
+        for (size_t j = i+1; j < n; j++)
+        {
+            if (a[j]<small)
+            {
+                small=a[j];
+                pos=j;
+            }
+            
+        }
+        temp=a[i];
+        a[i]=a[pos];
+        a[pos]=temp;
+    }
+    cout<<"Sorted array is :\n";
+    for (size_t i = 0; i < n; i++)
+    {
+        cout<<a[i]<<"\n";
+    }
+    return 0;
+}
--- DSA/tempCodeRunnerFile.cpp
+++ DSA/tempCodeRunnerFile.cpp
@@ -1,2 +1,45 @@
- #include<iostream>
- 
+#include<iostream>
+using namespace std;
+int SelectionSort(int *,int);
+int main(int argc, char const *argv[])
+{
+    int arr[20],num;
+    cout<<"Enter Number of elem array";
+    cin>>num;
+    cout<<"Enter al the elements of the array";
+    for (size_t i = 0; i < num; i++)
+    {
+        cin>>arr[i];
+    }
+    SelectionSort(arr,num);
+    return 0;
+}
+int SelectionSort(int a[],int n)
+{ 
+    int small,pos,temp;
+    for (size_t i = 0; i < n; i++)
+    {
+        small = a[i];
+        pos=i;
+        for (size_t j = i+1; j < n; j++)
+        {
+            if (a[j] < small)
+            {
+                small=a[j];
+                pos=j;
+            }
+            
+        }
+            temp=a[i];
+            a[i]=a[pos];
+            a[pos]=temp;  
+      
+        cout<<"array after pass ";
+        for (size_t i = 0; i < n; i++)
+         {
+            cout<<a[i]<<"\t";
+         }
+    }
+    
+     return 0;
+}
