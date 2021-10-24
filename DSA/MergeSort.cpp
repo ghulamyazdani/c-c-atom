a705ecbe40a61c0618176737a2b0ab8e0920cf45
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2021-10-25 01:02:36 +0530

    tt

--- DSA/MergeSort.cpp
+++ DSA/MergeSort.cpp
@@ -1,48 +1,52 @@
 #include <iostream>
 using namespace std;
-int merge(int a[], int l, int mid, int h)
+void merge(int a[], int l, int mid, int h)
 {
     int i = l;
     int j = mid + 1, k = l, c[100];
 
-    while (i < mid && j < h)
+    while (i <= mid && j <= h)
     {
         if (a[i] < a[j])
         {
             c[k++] = a[i++];
         }
+        else
+        {
+            c[k++] = a[j++];
+        }
     }
-    while (i < mid)
+    while (i <= mid)
     {
         c[k++] = a[i++];
     }
-    while (j < h)
+    while (j <= h)
     {
         c[k++] = a[j++];
     }
-
-    for (k = 0; k < h; k++)
+    for (size_t i = l; i <= h ; i++)
     {
-        a[k] = c[k];
+        a[i]= c[i];
     }
+    
 }
 
-int mergeSort(int a[], int l, int h)
+void mergeSort(int a[], int l, int h)
 {
     int mid;
     if (l < h)
     {
-        mid = l + (h-1) / 2;
+        mid = (l + h) / 2;
         mergeSort(a, l, mid);
         mergeSort(a, mid + 1, h);
         merge(a, l, mid, h);
     }
 }
-int printArray(int a[], int h)
+void printArray(int a[], int h)
 {
-    for (size_t i = 0; i < h ; i++)
+    for (size_t i = 0; i < h; i++)
     {
-        cout<<a[i];
+        cout << a[i];
     }
     
 }
@@ -50,7 +54,8 @@ int main()
 {
     int a[] = {2, 6, 1, 9, 4, 3, 7};
     int l = 0, h = sizeof(a) / sizeof(a[0]);
-    
-    mergeSort(a, l, h);
+   
+    mergeSort(a, l, h-1);
     printArray(a, h);
+    return 0;
 }
--- DSA/tempCodeRunnerFile.cpp
+++ DSA/tempCodeRunnerFile.cpp
@@ -1,85 +1,63 @@
-// Merge sort in C++
 
-#include <iostream>
-using namespace std;
 
-// Merge two subarrays L and M into arr
-void merge(int arr[], int p, int q, int r) {
-  
-  // Create L ← A[p..q] and M ← A[q+1..r]
-  int n1 = q - p + 1;
-  int n2 = r - q;
+void merge(int A[], int mid, int low, int high)
+{
+    int i, j, k, B[100];
+    i = low;
+    j = mid + 1;
+    k = low;
 
-  int L[n1], M[n2];
-
-  for (int i = 0; i < n1; i++)
-    L[i] = arr[p + i];
-  for (int j = 0; j < n2; j++)
-    M[j] = arr[q + 1 + j];
-
-  // Maintain current index of sub-arrays and main array
-  int i, j, k;
-  i = 0;
-  j = 0;
-  k = p;
-
-  // Until we reach either end of either L or M, pick larger among
-  // elements L and M and place them in the correct position at A[p..r]
-  while (i < n1 && j < n2) {
-    if (L[i] <= M[j]) {
-      arr[k] = L[i];
-      i++;
-    } else {
-      arr[k] = M[j];
-      j++;
+    while (i <= mid && j <= high)
+    {
+        if (A[i] < A[j])
+        {
+            B[k] = A[i];
+            i++;
+            k++;
+        }
+        else
+        {
+            B[k] = A[j];
+            j++;
+            k++;
+        }
     }
-    k++;
-  }
-
-  // When we run out of elements in either L or M,
-  // pick up the remaining elements and put in A[p..r]
-  while (i < n1) {
-    arr[k] = L[i];
-    i++;
-    k++;
-  }
-
-  while (j < n2) {
-    arr[k] = M[j];
-    j++;
-    k++;
-  }
+    while (i <= mid)
+    {
+        B[k] = A[i];
+        k++;
+        i++;
+    }
+    while (j <= high)
+    {
+        B[k] = A[j];
+        k++;
+        j++;
+    }
+    for (int i = low; i <= high; i++)
+    {
+        A[i] = B[i];
+    }
+    
 }
 
-// Divide the array into two subarrays, sort them and merge them
-void mergeSort(int arr[], int l, int r) {
-  if (l < r) {
-    // m is the point where the array is divided into two subarrays
-    int m = l + (r - l) / 2;
-
-    mergeSort(arr, l, m);
-    mergeSort(arr, m + 1, r);
-
-    // Merge the sorted subarrays
-    merge(arr, l, m, r);
-  }
+void mergeSort(int A[], int low, int high){
+    int mid; 
+    if(low<high){
+        mid = (low + high) /2;
+        mergeSort(A, low, mid);
+        mergeSort(A, mid+1, high);
+        merge(A, mid, low, high);
+    }
 }
 
-// Print the array
-void printArray(int arr[], int size) {
-  for (int i = 0; i < size; i++)
-    cout << arr[i] << " ";
-  cout << endl;
-}
-
-// Driver program
-int main() {
-  int arr[] = {6, 5, 12, 10, 9, 1};
-  int size = sizeof(arr) / sizeof(arr[0]);
-
-  mergeSort(arr, 0, size - 1);
-
-  cout << "Sorted array: \n";
-  printArray(arr, size);
-  return 0;
+int main()
+{
+    // int A[] = {9, 14, 4, 8, 7, 5, 6};
+    int A[] = {9, 1, 4, 14, 4, 15, 6};
+    int n = 7;
+    printArray(A, n);
+    mergeSort(A, 0, 6);
+    printArray(A, n);
+    return 0;
 }
