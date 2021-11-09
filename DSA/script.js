c68bdddd9ae8c063735fb0712f1fdeae28a7cfb5
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2021-11-09 22:11:42 +0530

    prac

--- DSA/script.js
+++ DSA/script.js
@@ -13,3 +13,22 @@ function getDate() {
     var today = year + "-" + month + "-" + day;
     return today;
 }
+
+//funtion to get the current time       
+function getTime() {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    var time = hours + ":" + minutes + ":" + seconds;
+    return time;
+}
